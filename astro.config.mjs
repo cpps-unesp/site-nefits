3cfaf740c487ed4361ee9f5ba56a9525d2d29c83
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2024-04-08 20:17:47 -0300

    move path img5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
