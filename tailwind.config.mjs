7e0c6f2079de1253e3b0271cb4f23bf80681d56d
Rafael de Almeida <rafaelrdealmeida@gmail.com>
2025-04-30 20:45:13 -0300

    Pagina de noticias refatorada. Vale ajustar ainda o slug

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,20 +1,20 @@
-/** @type {import('tailwindcss').Config} */
+import typography from '@tailwindcss/typography'
+
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-	  extend: {
-		fontFamily: {
-		  sans: ['Comfortaa', 'sans-serif'],       // Para uso global como font-sans
-		  comfortaa: ['Comfortaa', 'sans-serif'],  // Alternativa opcional como font-comfortaa
-		},
-		colors: {
-		  primary: '#ab090e',
-		  neutral: '#656565',
-		  light: '#eeeeee',
-		  light2: '#f1e6e6',
-		},
-	  },
-	},
-	plugins: [],
-  }
-  
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Comfortaa', 'sans-serif'],
+        comfortaa: ['Comfortaa', 'sans-serif'],
+      },
+      colors: {
+        primary: '#ab090e',
+        neutral: '#656565',
+        light: '#eeeeee',
+        light2: '#f1e6e6',
+      },
+    },
+  },
+  plugins: [typography],
+}
